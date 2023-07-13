a092e4cce38b59a59e3ad9a0105abe7db8d9e93f
montyaction <montyaction@hotmail.com>
2023-07-13 17:25:54 +0530

    update the aap component

--- src/App.js
+++ src/App.js
@@ -9,44 +9,42 @@ function App() {
   const [long, setLong] = useState([]);
   const [data, setData] = useState([]);
 
-  const weatherData = {
-    clouds: {all:75},
-    main: {
-      temp: 25.09, feels_like: 25.95, temp_min: 25.09, temp_max: 25.09, pressure: 1007, humidity: 88
-    },
-    name: "Indore",
-    sys: {type: 1, id: 9067, country: 'IN', sunrise: 1689207559, sunset: 1689255884},
-    timezone: 19800,
-    visibility: 4000,
-    weather: [{id: 721, main: 'Haze', description: 'haze', icon: '50d'}],
-    wind:{speed:3.6,"deg":160}
-  }
+  // const weatherData = {
+  //   clouds: {all:75},
+  //   main: {
+  //     temp: 25.09, feels_like: 25.95, temp_min: 25.09, temp_max: 25.09, pressure: 1007, humidity: 88
+  //   },
+  //   name: "Indore",
+  //   sys: {type: 1, id: 9067, country: 'IN', sunrise: 1689207559, sunset: 1689255884},
+  //   timezone: 19800,
+  //   visibility: 4000,
+  //   weather: [{id: 721, main: 'Haze', description: 'haze', icon: '50d'}],
+  //   wind:{speed:3.6,"deg":160}
+  // }
 
   
-
   useEffect(()=> {
-    // const fetchData = async () => {
+    const fetchData = async () => {
       navigator.geolocation.getCurrentPosition(position => {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
   
-    //   await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-    //   .then(res => res.json())
-    //   .then(result => {
-    //     setData(result)
-    //     console.log(result);
-    //   });
-    // }
-    // fetchData();
-    setData(weatherData);
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
   }, [lat, long]);
-
+  
 
   return (
     <div className="App">
       {
-        (typeof data.main === 'undefined')
+        (typeof data.main != 'undefined')
         ? (<Weather weatherData={data}/>)
         : (<div><Dimmer active><Loader><h1>Loading...</h1></Loader></Dimmer></div>)
       }
